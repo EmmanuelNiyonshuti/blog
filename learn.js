91391efb25bd3bedf15712aff8ee9defdfec60f4
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2025-09-10 22:51:56 +0200

    Remove deleted learn.js file

--- learn.js
+++
@@ -1,9 +0,0 @@
-import { fileURLToPath } from 'url';
-
-// const __filename = fileURLToPath(import.meta.url);
-
-// new versions on nodejs
-const dname = import.meta.dirname;
-console.log("dname:", dname)
-console.log("dirname:", import.meta.dirname);
-console.log("filename:", import.meta.filename);
