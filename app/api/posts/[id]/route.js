a647ed5cdd5a9b6c512d6cc191a220f7f60d92f8
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2025-11-23 16:05:42 +0200

    chore: update get post by id route proxy

--- app/api/posts/[id]/route.js
+++ app/api/posts/[id]/route.js
@@ -3,42 +3,8 @@ import { NextResponse } from "next/server";
 
 export const API_BASE_URL = process.env.API_URL || process.env.NEXT_PUBLIC_API_URL || 'https://blog-backend-2u9m.onrender.com/api';
 
-
-// POST - Create new post
-export async function POST(request) {
-    try {
-        const cookieStore = await cookies();
-        const token = cookieStore.get('token')?.value;
-        
-        if (!token) {
-            return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-        }
-        const data = await request.json();
-        const response = await fetch(`${API_BASE_URL}/posts`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify(data)
-        });
-        
-        if (!response.ok) {
-            const error = await response.json();
-            return NextResponse.json(error, { status: response.status });
-        }
-        const newPost = (await response.json()).data;
-        return NextResponse.json(newPost);
-    } catch (error) {
-        console.error('Error creating post:', error);
-        return NextResponse.json({ error: 'Failed to create post' }, { status: 500 });
-    }
-}
-
-// get post by id
 export async function GET(request, { params }) {
-    // The 'params' object contains the dynamic route parameters!
-    const { id } = await params; // This is your post ID from the URL
+    const { id } = await params;
     try {
         const cookieStore = await cookies();
         const token = cookieStore.get('token')?.value;
