0d22a343f4da0f21fc06e9c113c7b049715d5b49
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2026-02-18 17:21:02 +0200

    update next .mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,12 @@
-import { withSentryConfig } from '@sentry/nextjs';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // output: 'standalone',
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
-      {
-        protocol: 'https',
-        hostname: 'cdn-images-1.medium.com',
-      }
     ]
   },
-  // Turbopack experimental features
-  // experimental: {
-  //   turbo: {
-  //     // Turbopack rules if needed in future
-  //   },
-  // },
 };
-
-// Export with Sentry configuration
-export default withSentryConfig(nextConfig, {
-  org: "emmanuel-b3",
-  project: "my-blog",
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  
-  webpack: {
-    automaticVercelMonitors: true,
-    treeshake: {
-      removeDebugLogging: true,
-    },
-  }
-});
