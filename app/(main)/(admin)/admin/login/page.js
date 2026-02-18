bce8d13edc268792239559a7c9f577100b2dfa19
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2026-02-18 16:47:38 +0200

    migrate to serving static mdx

--- app/(main)/(admin)/admin/login/page.js
+++
@@ -1,9 +0,0 @@
-import LoginForm from "./login-form";
-
-export default function AdminLogin() {
-  return (
-    <div className="flex items-center justify-center bg-gray-50 dark:bg-gray-900 px-4 py-8">
-      <LoginForm />
-    </div>
-  );
-}
--- prisma/migrations/20251129174128_add/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."comments" ADD COLUMN     "parentId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "public"."comments" ADD CONSTRAINT "comments_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "public"."comments"("id") ON DELETE CASCADE ON UPDATE CASCADE;
