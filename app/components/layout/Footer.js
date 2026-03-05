af89b4aef72cf34ebc884b40dcdcbaf1e49ceda1
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2026-03-05 15:51:25 +0200

    add source code in footer

--- app/components/layout/Footer.js
+++ app/components/layout/Footer.js
@@ -4,6 +4,7 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex items-center justify-center">
           <p className="text-xs text-gray-600 dark:text-gray-400">© 2024 - {new Date().getFullYear()} NIYONSHUTI Emmanuel. All rights reserved.</p>
+          <a className="flex justify-end ml-3 text-xs text-blue-600 dark:text-blue-400" href="https://github.com/EmmanuelNiyonshuti/blog" target="_blank" rel="noopener noreferrer">source code</a>
         </div>
       </div>
     </footer>
