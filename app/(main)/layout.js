103c20536c46c8e4dac0b3d78181018926b99400
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2026-03-21 18:06:02 +0200

    flex-grow to grow

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -35,7 +35,7 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange>
         <div className="min-h-screen flex flex-col">
           <Navbar />
-          <main className="flex-grow">
+          <main className="grow">
             {children}
           </main>
           <Footer />
