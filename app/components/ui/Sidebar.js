b3a3452c5237f626a446dc70b6df5ddf63a23a79
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2026-05-23 11:25:47 +0200

    update sidebar categories

--- app/components/ui/Sidebar.js
+++ app/components/ui/Sidebar.js
@@ -3,15 +3,17 @@ import CategoriesSection from "../blog/CategoriesSection"
 import Welcome from "./Welcome"
 
 const Sidebar = ({categories}) => {
-  return (
-  <aside className="hidden lg:block lg:col-span-1 self-stretch">
-    <div className="lg:sticky lg:top-8 flex flex-col gap-16">
-        <SearchBar />
-        <CategoriesSection categories={categories} />
-        <Welcome />
-    </div>
-  </aside>
-  )
+
+    return (
+        <aside className="hidden lg:block lg:col-span-1 self-stretch">
+            <div className="lg:sticky lg:top-8 flex flex-col gap-16">
+                <SearchBar />
+                <CategoriesSection categories={categories} />
+                <Welcome />
+            </div>
+        </aside>
+    )
 }
 
-export default Sidebar
+export default Sidebar;
+
